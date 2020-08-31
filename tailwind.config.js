e283e0d690bae35bcab7bb0050fe48b705fe751d
Choton <choton654@gmail.com>
2020-08-31 14:21:55 +0530

    taliwind css

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#ff6363',
+        secondary: {
+          100: '#e3e2d5',
+          200: '#888883',
+        },
+      },
+      fontFamily: {
+        body: ['Roboto'],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
